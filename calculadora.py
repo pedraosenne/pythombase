68647caa4681bc1b3ff24a3caaca1dffccfd4088
pedro_senne <pedro.senne.santos@escola.pr.gov.br>
2026-02-09 14:11:34 +0000

    comit1

---
+++ calculadora.py
@@ -0,0 +1,20 @@
+num1 = int(input("Digite o primeiro numero"))
+num2 = int(input("Digite o primeiro numero"))
+
+soma = num1+num2
+
+print("A soma é : ",soma)
+
+num1 = int(input("Digite o primeiro numero"))
+num2 = int(input("Digite o primeiro numero"))
+
+soma = num1-num2
+
+print("A subitraçao é : ",subitracao)
+
+num1 = int(input("Digite o primeiro numero"))
+num2 = int(input("Digite o primeiro numero"))
+
+soma = num1+num2
+
+print("A divisão é : ",divisao)
---
+++ ola.py
@@ -0,0 +1,4 @@
+nome = input ("Digite seu nome:" )
+Sobrenome = input ("Digite seu Sobrenome:" )
+
+print ("Olá,", nome, Sobrenome, "Seja bem vindo" )
