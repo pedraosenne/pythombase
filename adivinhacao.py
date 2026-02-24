21672251f2ef58302dad7709340d4f0be23c5c14
pedro_senne <pedro.senne.santos@escola.pr.gov.br>
2026-02-24 12:31:29 +0000

    versao03

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,17 +1,24 @@
+import random
 print("*************************")
 print("****Jogo Adivinhação*****")
 print("*************************")
 
-numero_secreto = 69
+numero_secreto = random.randrange(1,101)
 total_tentativas = 5
 rodada = 1
 
 
-while (rodada <= total_tentativas):
+for rodada in range(1,total_tentativas + 1):
+
     print(f"Tentativa {rodada} de {total_tentativas}")
 
     chute_str = input("Digite o seu número: ")
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 67):
+      print("Voce deve digitart um numero entre 1 e 67")
+      continue
+   
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -25,7 +32,5 @@ while (rodada <= total_tentativas):
             print("O seu chute foi MAIOR que o número secreto.")
         elif (menor):
             print(" O seu chute foi MENOR que o número secreto.")
-
-    rodada = rodada + 1
 
 print("Fim de jogo!")
